db0cbdfa64153fbccdb3b9d8eeffa0e43e564dbd
AlejandroOliverosVera <alejandro.oliveros@alumnos.uv.cl>
2020-10-22 21:07:52 -0300

    test medio funcional

--- test.c
+++ test.c
@@ -2,27 +2,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdbool.h>
-
-void test(){
-        printf("yep");
-}
 
 void handler(int num){
-        write(STDOUT_FILENO, "La hora: super tarde",21);
-        test();
+    if (fork() == 0){
+        execl("/usr/bin/date","date",NULL);
+    }else{
+        write(STDOUT_FILENO, "Esperando señal SIGUSR1\n", 25);
+    }
 }
 
 void do_nothing(){
+    signal(SIGUSR1, handler);
+    printf("Esperando señal, PID = %d\n", getpid());
+    while(1){
         sleep(1);
+    }
 }
 
 int main (){
-	signal(SIGUSR1, handler);
-	bool signal_recieved = false;
-	printf("Programa hora ejecutandose, PID=%d.\nListo para recibir la senal SIGUSR1\n", getpid());
-	while(signal_recieved == false){
-		do_nothing();
-	}
-	return 0;
+    do_nothing();
+    return 0;
 }
+
